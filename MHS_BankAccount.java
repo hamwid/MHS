6cfb1cbbbf74bf53a056fe7e0fe52faa92032422
hamwid <hampus.widell@alleman.nu>
2019-01-04 08:53:58 +0100

    added BankAccount code

---
+++ MHS_BankAccount.java
@@ -0,0 +1,12 @@
+public class MHS_BankAccount{
+  public double accountBalance;
+  public String accountNumber;
+
+  public MHS_BankAccount (String iAccountNumber, double iAccountBalance){
+    accountBalance = iAccountBalance;
+    accountNumber  = iAccountNumber;
+  }
+  public void pay(double price){
+    accountBalance = accountBalance - price;
+  }
+}
